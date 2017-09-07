4c1b76df9988d9ae29342251faf5c3dc74fab4b4
odem20 <odem20@up.edu>
2017-09-07 16:29:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/odem20/carstereo_odem_mellmer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.odem20.carstereo_odem_mellmer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
